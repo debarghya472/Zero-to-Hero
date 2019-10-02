d0457f8521ba73f5cfbed320b18c3d9176703be1
debarghya472 <54908977+debarghya472@users.noreply.github.com>
2019-10-02 15:40:03 +0530

    calculator created

---
+++ GCC/calculator.cpp
@@ -0,0 +1,20 @@
+
+#include <iostream> 
+using namespace std; 
+
+// Main program 
+int main() 
+{ 
+	int a,b;
+	cout<<"enter two numbers:";
+	cin>>a,b;
+	int sum=a+b;
+	cout<<"sum="<<sum;
+	int sub=a-b;
+	cout<<"substraction="<<sub;
+	int mul=a*b;
+	cout<<"multiplication="<<mul;
+	int div=a/b;
+	cout<<"division="<<div;
+	return 0;
+	}
